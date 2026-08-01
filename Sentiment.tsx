f416f6c36f128d2e20037a3f3f999b8b89415adf
Dbams1 <darebamidele01@yahoo.co.uk>
2026-08-01 12:11:07 -0700

    Delete Sentiment.tsx

--- Sentiment.tsx
+++
@@ -1,6 +0,0 @@
-import MarketOverview from "@/components/MarketOverview";
-import Sentiment from "@/components/Sentiment";
-import StockPicks from "@/components/StockPicks";
-export default function Page() {
-  return (<><MarketOverview /><Sentiment /><StockPicks /></>);
-}
